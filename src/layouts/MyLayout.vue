9ef4156370bff71da924198bb549730265f688c7
ming <xiemingzhi@gmail.com>
2020-01-15 15:08:14 +0800

    Add footer tabs

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -117,6 +117,14 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer elevated>
+      <q-tabs v-model="tab">
+        <q-tab name="mails" icon="mail" label="Mails" />
+        <q-tab name="alarms" icon="alarm" label="Alarms" />
+        <q-tab name="movies" icon="movie" label="Movies" />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -126,7 +134,8 @@ export default {
 
   data() {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
+      tab: "mails"
     };
   },
   methods: {
